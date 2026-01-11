41484ac1218190c1b2188502388b71e8bc7b9a3b
Anton Zagarinskiy <anton.z721@yandex.ru>
2026-01-11 22:18:19 +0400

    Add files via upload

---
+++ variables.py
@@ -0,0 +1,86 @@
+# задача 2.1
+#   задание 1
+# movie_name = "Harry Potter and the Sorcerer's Stone"
+# year_published = 2001
+# kinopoisk_rate = 8.3
+# main_actors = ["Daniel Radcliffe", "Rupert Grint", "Emma Watson"]
+# print (movie_name)
+# print (year_published)
+# print(kinopoisk_rate)
+# print(main_actors)
+# print ("название фильма:",  movie_name)
+# print ("год публикации", year_published)
+# print("рейтинг кинопоиска", kinopoisk_rate)
+# print("главные герои:", main_actors)
+#
+# # задание 2
+#
+# name = "Tom"
+# print(name)
+# name = "Bob"
+# print("изменили имя на", name)
+#
+# # задание 3
+# a = b = c = 10
+# print ("до изменения: а=", a,"b=", b, "c=", c)
+# c += 5
+# print ("после: а=", a,"b=", b, "c=", c)
+#
+# # задание 4
+#
+# a, b, c = "Tom", "Bob", "Jake"
+# print ("a =", a, "b =", b, "c =", c)
+
+# задание 5
+
+# a = 10
+# b = 5.5
+#
+# sum_result = a + b
+# print("sum_result:", sum_result)
+#
+# sub_result = a - b
+# print("sub_result:", sub_result)
+#
+# mul_result = a * b
+# print("mul_result:", mul_result)
+#
+# div_result = a / b
+# print("div_result:", div_result)
+#
+# pow_result = a ** b
+# print("pow_result:", pow_result)
+#
+# floor_div_result = a // b
+# print("floor_div_result:", floor_div_result)
+#
+# mod_result = a % b
+# print("mod_result:", mod_result)
+
+# задание 6
+
+# c = 6
+# print ("c=", c)
+# c += 3
+# print ("c=", c)
+# c -= 2
+# print ("c=", c)
+# c /= 2
+# print ("c=", c)
+# c *= 4
+# print ("c=", c)
+
+#  задание 7
+
+# name = "Python"
+# x = "Hello_"
+#
+# x += name
+# print("x =", x)
+#
+# name *= 3
+# print("name =", name)
+#
+# y = 10
+# z = int("10")
+# print(y + z)
