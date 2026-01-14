f728be3ec429fe01a30322a9121c2215922c96f4
Anton Zagarinskiy <anton.z721@yandex.ru>
2026-01-14 06:37:47 +0400

    Add files via upload

---
+++ operations.py
@@ -0,0 +1,202 @@
+# Задача 2.3
+
+# Зажание 1
+
+# a = [4, 5, 6]
+# b = [4, 5, 6]
+#
+# a == b
+# a is b
+#
+# print("a == b", a == b)
+# print("a is b", a is b)
+# # print(id(a))
+# # print(id(b))
+
+# a = 15
+# b = 15
+#
+# a != b
+# a is b
+#
+# print("a != b", a != b)
+# print("a is b", a is b)
+
+# a = "Python"
+# b = "Python"
+#
+# a == b
+# a is b
+#
+# print("a == b", a == b)
+# print("a is b", a is b)
+
+# a = 200
+# b = 150
+# b += 50
+#
+# a == b
+# a is b
+#
+# print("a == b", a == b)
+# print("a is b", a is b)
+
+# a = 300
+# b = 250
+# b += 50
+#
+# a == b
+# a is b
+#
+# print("a == b", a == b)
+# print("a is b", a is b)
+
+# a = ["10", "20", "30", "40"]
+# b = "20"
+# c = 30
+#
+# b in a
+# not(c not in a)
+#
+# print("b in a", b in a)
+# print("not(c not in a)", not(c not in a))
+
+# Зажание 2
+
+# x = 10
+#
+# x > 8 and x > 25
+# x > 8 and x < 25
+# x < 8 and x < 25
+# x < 8 and x > 25
+#
+# print("x > 8 and x > 25", x > 8 and x > 25)
+# print("x > 8 and x < 25", x > 8 and x < 25)
+# print("x < 8 and x < 25", x < 8 and x < 25)
+# print("x < 8 and x > 25", x < 8 and x > 25)
+
+# x = 10
+#
+# x > 8 or x > 25
+# x > 8 or x < 25
+# x < 8 or x < 25
+# x < 8 or x > 25
+#
+# print("x > 8 or x > 25", x > 8 or x > 25)
+# print("x > 8 or x < 25", x > 8 or x < 25)
+# print("x < 8 or x < 25", x < 8 or x < 25)
+# print("x < 8 or x > 25", x < 8 or x > 25)
+
+# x = 10
+#
+# not(x < 8 and x < 25)
+# not(x > 8 or x < 25)
+#
+# print("not(x < 8 and x < 25)", not(x < 8 and x < 25))
+# print("not(x > 8 or x < 25)", not(x > 8 or x < 25))
+
+# x = 4
+
+# x >= 4 and x > 16
+# x > 4 and x < 16
+# x <= 4 and x < 16
+# x < 4 and x > 16
+#
+# print("x >= 4 and x > 16", x >= 4 and x > 16)
+# print("x > 4 and x < 16", x > 4 and x < 16)
+# print("x <= 4 and x < 16", x <= 4 and x < 16)
+# print("x < 4 and x > 16", x < 4 and x > 16)
+
+# x >= 4 or x > 16
+# x > 4 or x < 16
+# x < 4 or x < 16
+# x <= 4 or x > 16
+#
+# print("x >= 4 or x > 16", x >= 4 or x > 16)
+# print("x > 4 or x < 16", x > 4 or x < 16)
+# print("x < 4 or x < 16", x < 4 or x < 16)
+# print("x <= 4 or x > 16",x <= 4 or x > 16)
+#
+# not(x > 4 and x < 16)
+# not(x <= 4 or x < 16)
+#
+# print("not(x > 4 and x < 16)", not(x > 4 and x < 16))
+# print("not(x <= 4 or x < 16)", not(x <= 4 or x < 16))
+#
+# a = "hello"
+# b = ""
+#
+# a and b
+# a or b
+#
+# print("a and b", a and b)
+# print("a or b", a or b)
+#
+# a = [1, 2, 3]
+# b = [4, 5, 6]
+#
+# a and b
+# a or b
+#
+# print("b and a", b and a)
+# print("b or a", b or a)
+
+# Зажание 3
+
+# a = True
+# b = False
+#
+# a and b or not a and not b
+# not (a or b) and (a or not b)
+# (a and not b) or (b and not a)
+#
+# print("a and b or not a and not b", a and b or not a and not b)
+# print("not (a or b) and (a or not b)", not (a or b) and (a or not b))
+# print("(a and not b) or (b and not a)", (a and not b) or (b and not a))
+
+# Зажание 4
+
+# x = 5
+# y = 10
+#
+# y > x * x or y >= 2 * x and x < y
+# print("y > x * x or y >= 2 * x and x < y", y > x * x or y >= 2 * x and x < y)
+#
+# x = 3
+# y = 7
+#
+# not (x * 2 == y or y - x < 2)
+# print("not (x * 2 == y or y - x < 2)", not (x * 2 == y or y - x < 2))
+
+# x = 4
+# y = 2
+#
+# x > y and y != 0 or x / y > 3
+# print("x > y and y != 0 or x / y > 3", x > y and y != 0 or x / y > 3)
+
+# Зажание 5
+
+# x = 10
+# y = [9, 10, 11, 12, 13, 14, 15, 16, 17, 18]
+#
+# x in y
+# print("x in y", x in y)
+
+# x = 60
+#
+# x < 23 or x > 60
+# print("x < 23 or x > 60", x < 23 or x > 60)
+
+# Зажание 6
+
+# a = 10
+# b = 10
+# x = (a is not None and b is not None) and (a > 0 or b > 0)
+# print ("x = ", x )
+
+
+#
+# numbers = input() for _ in input()
+# answer = num for num in numbers if num % 2 = 0 or num % 4 != 0
+# print(answer)
+
